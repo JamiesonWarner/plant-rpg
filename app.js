577f2fd8ebbe9debdf168dfb452577b0b6b8caa0
Jamie Warner <jamiesonwarner@gmail.com>
2016-02-20 01:36:40 -0600

    for max

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 
-var bbox = {xl: 0, xr:  800, yt: 0, yb: 600};
+var bbox = {xl: 0, xr: 600, yt: 0, yb: 600};
 var game = new Phaser.Game(bbox.xr, bbox.yb, Phaser.AUTO, 'phaser-example', {create: create});
 
 function create() {
   var grid = new Grid();
   var plant = new Plant();
 
+  game.stage.backgroundColor = '#535353';
 
-  var vDiagram = getVoronoiDiagram(plant);
+  // var vDiagram = getVoronoiDiagram(plant);
 
   var graphics = game.add.graphics(0, 0);
   graphics.lineStyle(10, 0xffd900, 1);
 
-  for(edge in vDiagram.edges){
-  	graphics.moveTo(edge.va.x,edge.va.y);
-  	graphics.lintTo(edge.vb.x, edge.vb.y);
-  }
+  // for(edge in vDiagram.edges){
+  // 	graphics.moveTo(edge.va.x,edge.va.y);
+  // 	graphics.lintTo(edge.vb.x, edge.vb.y);
+  // }
 
 
   plant.cells
--- grid.js
+++ grid.js
@@ -1,14 +1,14 @@
 function Grid() {
-  var RANGE = 50;
-  var CELL_SIZE = 5;
+  var RANGE = 60;
+  var CELL_SIZE = 10;
 
   var graphics = game.add.graphics(0, 0);
   graphics.lineStyle(1, 0xffd9ff, 1);
-  for (var i = -RANGE; i < RANGE; i++) {
+  for (var i = 0; i < RANGE; i++) {
     graphics.moveTo(i * CELL_SIZE, RANGE * CELL_SIZE);
     graphics.lineTo(i * CELL_SIZE, -RANGE * CELL_SIZE);
   };
-  for (var i = -RANGE; i < RANGE; i++) {
+  for (var i = 0; i < RANGE; i++) {
     graphics.moveTo(RANGE * CELL_SIZE, i * CELL_SIZE);
     graphics.lineTo(-RANGE * CELL_SIZE, i * CELL_SIZE);
   };
