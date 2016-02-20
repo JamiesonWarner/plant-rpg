19b7c8f032945e0495f5943ad7a650244c81e1af
Jamie Warner <jamiesonwarner@gmail.com>
2016-02-20 00:46:44 -0600

    some code

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', {create: create});
 
 function create() {
+  var plant = new Plant();
+  plant.cells
+  plant.envCells
+// Game world will be 100x100 blocks
+// Set up render loops
+}
+
+// Called once every second
+function tick() {
 
 }
---
+++ plant.js
@@ -0,0 +1,10 @@
+var plant = [[50,50],[50,49]],
+    env = [[49,50],[50,51],[51,50],[51,49],[50,49]];
+
+function Plant() {
+
+}
+
+function addCell(cell) {
+
+}
