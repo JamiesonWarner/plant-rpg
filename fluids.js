72f2acbf6fbcf3f746a58c083426304c85d16b5e
Jamie Warner <jamiesonwarner@gmail.com>
2016-02-20 14:47:36 -0600

    fluids

---
+++ fluids.js
@@ -0,0 +1,21 @@
+function calculateTick() {
+
+}
+
+/*
+Assigns ptd to each cell in cells
+*/
+function passiveTransportDeltas(plant) {
+}
+
+/*
+Assigns atd to each cell in cells
+*/
+function activeTransportDeltas(plant) {
+}
+
+
+
+function massDifferential(cell) {
+
+}
