afbaf4ea84bc6f5fe6ab5f910de544925c065c5a
Jamie Warner <jamiesonwarner@gmail.com>
2016-02-20 01:05:49 -0600

    use dictionaries for cells

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', {create: create});
 
 function create() {
+  var grid = new Grid();
   var plant = new Plant();
+
   plant.cells
   plant.envCells
 // Game world will be 100x100 blocks
@@ -12,3 +14,5 @@ function create() {
 function tick() {
 
 }
+
+function
